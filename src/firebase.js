d2b722b7baeb336ed27a5eae33a7f43cd7129f4e
yakchacha <90446640+yakchacha@users.noreply.github.com>
2025-08-29 17:01:28 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "너의_API_KEY",
-  authDomain: "너의_PROJECT_ID.firebaseapp.com",
-  projectId: "너의_PROJECT_ID",
-  storageBucket: "너의_PROJECT_ID.appspot.com",
-  messagingSenderId: "너의_ID",
-  appId: "너의_APP_ID"
+  apiKey: "AIzaSyD8mqTmsrPpZ78TnlG2futeve5hEc3aEFI",            // 복사해온 값 붙여넣기
+  authDomain: "lunchroulette-4758c.firebaseapp.com",
+  projectId: "lunchroulette-4758c",
+  storageBucket: "lunchroulette-4758c.firebasestorage.app",
+  messagingSenderId: "312740196547",
+  appId: "1:312740196547:web:c05ecc89cfeaae16453276"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export default app;
+export const db = getFirestore(app);
